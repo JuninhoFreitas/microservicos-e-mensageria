c70fd1698fbaf7a72b79a7544b61ec2a22149af9
Juninho <juninho@pop-os.localdomain>
2022-11-17 15:55:51 -0300

    added few archives

--- inventory/index.js
+++ inventory/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (_req,res)=>{
+    res.status(200).send(process.env.MICROSERVICE_NAME || 'OK');
+})
 
 app.listen(PORT, ()=>{
     console.log(`Running microservice of ${process.env.MICROSERVICE_NAME||'example-app'} in localhost:${PORT}/`)
